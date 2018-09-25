3bb5a066c11319cb240a04a485980ef1aa7c4fe8
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-09-25 10:58:00 -0400

    - WarpWorks/warpjs#146: Styling login window.

--- client/portal/login/index.js
+++ client/portal/login/index.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird');
 
-const proxy = require('./../../lib/proxy');
-const toast = require('./../../lib/toast');
+const proxy = require('./../../../lib/proxy');
+const toast = require('./../../../lib/toast');
 
 function findInput($, element, fieldName) {
     return $(element).closest('.warpjs-login-form').find(`input[name="${fieldName}"]`);
