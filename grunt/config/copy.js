db5abdd0cc62f7a82888a765e60128e8109ee3d0
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-10-22 14:07:30 -0400

    - Added `tinymce`. - Refactored to not require full `warpjs` publish cycle when new version of `warpjs-utils` is published.

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -29,6 +29,24 @@ module.exports = {
         ]
     },
 
+    'tinymce': {
+        files: [
+            filesConfig(jsFolder, [{ packageName: 'tinymce', filePath: 'tinymce*.js' }]),
+            filesConfig(path.join(jsFolder, 'themes', 'modern'), [{ packageName: 'tinymce', filePath: 'themes/modern/*.js' }]),
+            {
+                expand: true,
+                flatten: false,
+                dest: jsFolder,
+                cwd: path.dirname(require.resolve('tinymce/package.json')),
+                src: [
+                    'skins/**/*',
+                    'themes/**/*',
+                    'plugins/**/*'
+                ]
+            }
+        ]
+    },
+
     'toast': {
         files: [
             filesConfig(jsFolder, [{ packageName: 'jquery-toast-plugin', filePath: 'dist/*.js' }]),
@@ -42,7 +60,8 @@ module.exports = {
                 { packageName: 'react', filePath: 'umd/react.production.min.js' },
                 { packageName: 'react-dom', filePath: 'umd/react-dom.production.min.js' },
                 { packageName: 'react-bootstrap', filePath: 'dist/react-bootstrap.min.js' },
-                { packageName: 'react-redux', filePath: 'dist/react-redux.min.js' }
+                { packageName: 'react-redux', filePath: 'dist/react-redux.min.js' },
+                { packageName: 'redux', filePath: 'dist/redux.min.js' }
             ])
         ]
     }
